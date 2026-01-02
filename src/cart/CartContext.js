7cf141d616ffa3ee32f855c9d0e1fcefd2d9d7be
DESKTOP-RVB94N1\Vijay <vijay.prajapati@onedigital.com>
2026-01-02 09:15:55 +0530

    Release build

--- src/cart/CartContext.js
+++ src/cart/CartContext.js
@@ -1,4 +1,4 @@
-import { createContext, useState, useContext, useEffect } from "react";
+import { createContext, useState, useContext } from "react";
 
 const CartContext = createContext();
 
--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,5 +1,5 @@
 export const CURRENCY_CODE = "$";
-export const BASEURL_CATEGORY = 'http://localhost:5273/api/v1/Category/';
-export const BASEURL_PRODUCT = 'http://localhost:5273/api/v1/Product/';
-export const BASEURL_SUBCATEGORY = 'http://localhost:5273/api/v1/SubCategory/';
+export const BASEURL_CATEGORY = 'https://jiyazon.com/api/v1/Category/';
+export const BASEURL_PRODUCT = 'https://jiyazon.com/api/v1/Product/';
+export const BASEURL_SUBCATEGORY = 'https://jiyazon.com/api/v1/SubCategory/';
 
